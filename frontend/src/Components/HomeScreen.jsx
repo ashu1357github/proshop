15102ebd3a23a7d752e362155b021bc44733f39d
ashu <ashupatil1357@gmail.com>
2024-10-15 09:12:38 +0530

    Initial Commit

---
+++ frontend/src/Components/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Row,Col } from 'react-bootstrap'
+import products from '../products'
+import products from './../products';
+
+const HomeScreen = () => {
+  return (
+   <div>
+<h1> Latest Products</h1>
+<Row>
+    
+</Row>
+
+
+   </div>
+  )
+}
+export default HomeScreen
